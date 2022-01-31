e7db49481c1d67a45dedee0c98cff5a202a8638e
Jens Reimann <jreimann@redhat.com>
2022-01-31 09:17:17 +0100

    Include non-null

--- src/main/java/io/drogue/agent/opcua/state/State.java
+++ src/main/java/io/drogue/agent/opcua/state/State.java
@@ -2,8 +2,11 @@ package io.drogue.agent.opcua.state;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import io.drogue.agent.opcua.Bridge;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class State {
     private final Bridge.ConnectionState connection;
     private final List<Item> items;
